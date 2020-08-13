dc5e968c0bde93bbf94005d3c0a35a4eb768502b
Aria <54335666+ASangarin@users.noreply.github.com>
2020-08-13 05:15:32 +0200

    added comments

--- examples/EntryRequirement.java
+++ examples/EntryRequirement.java
@@ -16,11 +16,15 @@ public class EntryRequirement implements TechRequirement {
 	
 	@Override
 	public boolean met(TechEntry e, HumanEntity p) {
+		// Checks if the player has unlocked the specific entry.
+		// If yes, this requirement is met!
 		return PlayerData.get(p.getUniqueId()).hasEntry(tree + "." + entry);
 	}
 
 	@Override
 	public String display(TechEntry e, HumanEntity p, boolean isUnlocked) {	
+		// Uses the format manager to properly display this requirement.
+		// The string returned is the String that will replace the placeholder in the lore.
 		StringBuilder format = new StringBuilder(TTPlugin.plugin.findEntry(tree + "." + entry, e).getName());
 		if(!tree.equalsIgnoreCase(e.getTechTreeId()))
 			format.append(" (" + TechDatabase.get().getTree(tree).getDisplayName() + ")");
@@ -29,12 +33,19 @@ public class EntryRequirement implements TechRequirement {
 	}
 
 	@Override
-	public void fulfill(TechEntry e, HumanEntity p) {}
+	public void fulfill(TechEntry e, HumanEntity p) {
+		// This method gets called when the player
+		// Unlocks an entry with this requirement.
+		// Generally used to take required items / money,
+		// etc. from the player, hence the name 'fulfill'
+	}
 
 	@Override
 	public TechRequirement setup(TechEntry e, LineConfig c) {
+		// Loads data from the LineConfig
 		tree = c.getString("tree", e.getTechTreeId());
 		entry = c.getString("path");
+		// You would almost always want to "return this;" here.
 		return this;
 	}
 }
