3f9faeec15907b5a245c922e18c3193362ab73fd
Aria <54335666+ASangarin@users.noreply.github.com>
2020-09-23 22:39:05 +0200

    Update EntryRequirement.java

--- examples/EntryRequirement.java
+++ examples/EntryRequirement.java
@@ -1,6 +1,6 @@
 package eu.asangarin.tt.api.requirements;
 
-import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 
 import eu.asangarin.aria.utils.LineConfig;
 import eu.asangarin.tt.TTPlugin;
@@ -15,14 +15,14 @@ public class EntryRequirement implements TechRequirement {
 	private String entry;
 	
 	@Override
-	public boolean met(TechEntry e, HumanEntity p) {
+	public boolean met(TechEntry e, Player p) {
 		// Checks if the player has unlocked the specific entry.
 		// If yes, this requirement is met!
-		return PlayerData.get(p.getUniqueId()).hasEntry(tree + "." + entry);
+		return PlayerData.get(p).hasEntry(tree + "." + entry);
 	}
 
 	@Override
-	public String display(TechEntry e, HumanEntity p, boolean isUnlocked) {	
+	public String display(TechEntry e, Player p, boolean isUnlocked) {	
 		// Uses the format manager to properly display this requirement.
 		// The string returned is the String that will replace the placeholder in the lore.
 		StringBuilder format = new StringBuilder(TTPlugin.plugin.findEntry(tree + "." + entry, e).getName());
@@ -33,7 +33,7 @@ public class EntryRequirement implements TechRequirement {
 	}
 
 	@Override
-	public void fulfill(TechEntry e, HumanEntity p) {
+	public void fulfill(TechEntry e, Player p) {
 		// This method gets called when the player
 		// Unlocks an entry with this requirement.
 		// Generally used to take required items / money,
@@ -41,11 +41,12 @@ public class EntryRequirement implements TechRequirement {
 	}
 
 	@Override
-	public TechRequirement setup(TechEntry e, LineConfig c) {
+	public boolean setup(TechEntry e, LineConfig c) {
 		// Loads data from the LineConfig
 		tree = c.getString("tree", e.getTechTreeId());
 		entry = c.getString("path");
-		// You would almost always want to "return this;" here.
-		return this;
+		// If you 'return false;' the reward
+		// will not be added to the tech entry
+		return true;
 	}
 }
