43b907af5fd7203bf9ab9975fdf6e12b96e5e300
Aria <54335666+ASangarin@users.noreply.github.com>
2020-09-23 22:37:52 +0200

    Update CommandReward.java

--- examples/CommandReward.java
+++ examples/CommandReward.java
@@ -13,8 +13,6 @@ public class CommandReward implements TechReward {
 
 	@Override
 	public void reward(HumanEntity p) {
-	  // This method is called when the entry is unlocked.
-		// Use this to grant the player the 'reward' itself!
 		String cmd = command.replace("{player}", p.getName());
 		if(console) Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
 		else Bukkit.dispatchCommand(p, cmd);
@@ -22,10 +20,43 @@ public class CommandReward implements TechReward {
 
 	@Override
 	public TechReward setup(TechEntry entry, LineConfig c) {
-	  // Loading data from the LineConfig!
 		command = c.getString("format");
 		console = c.getBoolean("console", false);
-		// You will almost always want to use "return this;" here
 		return this;
+	}
+}
+
+
+package eu.asangarin.tt.api.rewards;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import eu.asangarin.aria.utils.LineConfig;
+import eu.asangarin.tt.api.TechReward;
+import eu.asangarin.tt.data.TechEntry;
+
+public class CommandReward implements TechReward {
+	private String command;
+	private boolean console;
+
+	@Override
+	public void reward(Player p) {
+		// This method is called when the entry is unlocked.
+		// Use this to grant the player the 'reward' itself!
+		String cmd = command.replace("{player}", p.getName());
+		if(console) Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+		else Bukkit.dispatchCommand(p, cmd);
+	}
+
+	@Override
+	public boolean setup(TechEntry entry, LineConfig c) {
+		// Loading data from the LineConfig!
+		if(!c.contains("format")) return false;
+		command = c.getString("format");
+		console = c.getBoolean("console", false);
+		// If you 'return false;' the reward
+		// will not be added to the tech entry
+		return true;
 	}
 }
